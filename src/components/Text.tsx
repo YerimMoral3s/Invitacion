f8adbe9da377786531a4ec1ea715585d332e0a53
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-02-29 23:49:20 -0600

    refactor text

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,101 +1,31 @@
-import React, { CSSProperties } from 'react';
+import React, {
+  CSSProperties,
+  forwardRef,
+  HTMLAttributes,
+  ForwardRefRenderFunction,
+  RefObject,
+} from 'react';
 import styled from 'styled-components';
 
-interface TextProps extends React.HTMLAttributes<HTMLHeadingElement> {
+interface TextProps extends HTMLAttributes<HTMLElement> {
   text: string;
   style?: CSSProperties;
+  as?: keyof JSX.IntrinsicElements;
 }
 
-const StyledH1 = styled.h1<{ style?: CSSProperties }>`
+const StyledText = styled.div<{ style?: CSSProperties }>`
   font-family: jakarta-regular;
-  font-size: 40px;
 `;
-export const H1: React.FC<TextProps> = ({ text, style, ...rest }) => {
+
+const Text: ForwardRefRenderFunction<HTMLDivElement, TextProps> = (
+  { text, style, as: Element = 'div', ...rest },
+  ref,
+) => {
   return (
-    <StyledH1 style={style} {...rest}>
+    <StyledText as={Element} ref={ref} style={style} {...rest}>
       {text}
-    </StyledH1>
+    </StyledText>
   );
 };
 
-const StyledH2 = styled.h2<{ style?: CSSProperties }>`
-  font-family: jakarta-regular;
-  font-size: 32px;
-`;
-
-export const H2: React.FC<TextProps> = ({ text, style, ...rest }) => {
-  return (
-    <StyledH2 style={style} {...rest}>
-      {text}
-    </StyledH2>
-  );
-};
-
-const StyledH3 = styled.h3<{ style?: CSSProperties }>`
-  font-family: jakarta-regular;
-  font-size: 24px;
-`;
-
-export const H3: React.FC<TextProps> = ({ text, style, ...rest }) => {
-  return (
-    <StyledH3 style={style} {...rest}>
-      {text}
-    </StyledH3>
-  );
-};
-
-const StyledH4 = styled.h4<{ style?: CSSProperties }>`
-  font-family: jakarta-regular;
-  font-size: 20px;
-`;
-
-export const H4: React.FC<TextProps> = ({ text, style, ...rest }) => {
-  return (
-    <StyledH4 style={style} {...rest}>
-      {text}
-    </StyledH4>
-  );
-};
-
-const StyledP = styled.p<{ style?: CSSProperties }>`
-  font-family: jakarta-regular;
-  font-size: 16px;
-`;
-export const P: React.FC<TextProps> = ({ text, style, ...rest }) => {
-  return (
-    <StyledP style={style} {...rest}>
-      {text}
-    </StyledP>
-  );
-};
-
-const StyledSpan = styled.span<{ style?: CSSProperties }>`
-  font-family: jakarta-regular;
-  font-size: 16px;
-`;
-
-export const Span: React.FC<TextProps> = ({ text, style, ...rest }) => {
-  return (
-    <StyledSpan style={style} {...rest}>
-      {text}
-    </StyledSpan>
-  );
-};
-
-const StyledLabel = styled.label<{ style?: CSSProperties }>`
-  font-family: jakarta-regular;
-  font-size: 16px;
-`;
-
-interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
-  text: string;
-  style?: CSSProperties;
-}
-
-export const Label: React.FC<LabelProps> = ({ text, style, ...rest }) => {
-  return (
-    <StyledLabel style={style} {...rest}>
-      {text}
-    </StyledLabel>
-  );
-};
+export default forwardRef(Text);

83eb387768f1b700e3943d80abb50f79e1c75a60
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-03-13 22:41:27 -0600

    WIP

--- src/sections/PhotosAnimation.tsx
+++ src/sections/PhotosAnimation.tsx
@@ -6,7 +6,7 @@ import img2 from '../assets/images/irv-son-2.jpg';
 import img3 from '../assets/images/irv-son-3.jpg';
 import img4 from '../assets/images/irv-son-4.jpg';
 import img5 from '../assets/images/irv-son-5.jpg';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 type styledProps = {
   show: 'false' | 'true';
@@ -15,7 +15,8 @@ type styledProps = {
 const StyledPhotosAnimation = styled.div<styledProps>`
   position: static;
   top: 0px;
-  height: 300vh;
+  height: 280vh;
+  background-color: ${colors.champania};
 
   .proposal-img {
     width: 100%;
@@ -36,25 +37,47 @@ const StyledPhotosAnimation = styled.div<styledProps>`
 
 export default function PhotosAnimation() {
   const { scroll } = useScroll();
-  console.log(scroll);
+  const imageRef = useRef(null);
 
   useEffect(() => {
     const curtain = document.querySelector('.curtain');
 
     if (!curtain) return;
+    if (!imageRef.current) return;
 
     // get height of the curtain
     const { height } = curtain.getBoundingClientRect();
 
-    if (scroll > height - 100) {
-      console.log('scrolling');
+    if (scroll < height + 100) {
+      // change the image
+      imageRef.current.src = img1;
+    }
+
+    if (scroll > height + 200) {
+      // change the image
+      imageRef.current.src = img2;
+    }
+
+    if (scroll > height + 300) {
+      // change the image
+      imageRef.current.src = img3;
+    }
+
+    if (scroll > height + 400) {
+      // change the image
+      imageRef.current.src = img4;
+    }
+
+    if (scroll > height + 500) {
+      // change the image
+      imageRef.current.src = img5;
     }
   }, [scroll]);
 
   return (
     <>
       <StyledPhotosAnimation show={scroll > 50 ? 'true' : 'false'}>
-        <img className="proposal-img" src={img1} alt="proposal" />
+        <img ref={imageRef} className="proposal-img" alt="proposal" />
       </StyledPhotosAnimation>
     </>
   );

84558700821b30f6387c76beb3668c174faefcf5
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-04-17 23:36:24 -0600

    add new images

--- src/sections/PhotosAnimation.tsx
+++ src/sections/PhotosAnimation.tsx
@@ -6,7 +6,39 @@ import img2 from '../assets/images/irv-son-2.jpg';
 import img3 from '../assets/images/irv-son-3.jpg';
 import img4 from '../assets/images/irv-son-4.jpg';
 import img5 from '../assets/images/irv-son-5.jpg';
+import img6 from '../assets/images/irv-son-6.jpg';
+import img7 from '../assets/images/irv-son-7.jpg';
+// import images from 8 to 15
+import img8 from '../assets/images/irv-son-8.jpg';
+import img9 from '../assets/images/irv-son-9.jpg';
+import img10 from '../assets/images/irv-son-10.jpg';
+import img11 from '../assets/images/irv-son-11.jpg';
+import img12 from '../assets/images/irv-son-12.jpg';
+import img13 from '../assets/images/irv-son-13.jpg';
+import img14 from '../assets/images/irv-son-14.jpg';
+import img15 from '../assets/images/irv-son-15.jpg';
+
 import { useEffect, useRef } from 'react';
+
+const imagesArray = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+];
+
+const heigContainer = imagesArray.length * 30;
 
 type styledProps = {
   show: 'false' | 'true';
@@ -15,7 +47,7 @@ type styledProps = {
 const StyledPhotosAnimation = styled.div<styledProps>`
   position: static;
   top: 0px;
-  height: 280vh;
+  height: ${heigContainer}vh;
   background-color: ${colors.green};
 
   .proposal-img {
@@ -71,6 +103,56 @@ export default function PhotosAnimation() {
     if (scroll > height + 500) {
       // change the image
       imageRef.current.src = img5;
+    }
+
+    if (scroll > height + 600) {
+      // change the image
+      imageRef.current.src = img6;
+    }
+
+    if (scroll > height + 700) {
+      // change the image
+      imageRef.current.src = img7;
+    }
+
+    if (scroll > height + 800) {
+      // change the image
+      imageRef.current.src = img8;
+    }
+
+    if (scroll > height + 900) {
+      // change the image
+      imageRef.current.src = img9;
+    }
+
+    if (scroll > height + 1000) {
+      // change the image
+      imageRef.current.src = img10;
+    }
+
+    if (scroll > height + 1100) {
+      // change the image
+      imageRef.current.src = img11;
+    }
+
+    if (scroll > height + 1200) {
+      // change the image
+      imageRef.current.src = img12;
+    }
+
+    if (scroll > height + 1300) {
+      // change the image
+      imageRef.current.src = img13;
+    }
+
+    if (scroll > height + 1400) {
+      // change the image
+      imageRef.current.src = img14;
+    }
+
+    if (scroll > height + 1500) {
+      // change the image
+      imageRef.current.src = img15;
     }
   }, [scroll]);
 

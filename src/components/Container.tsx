97ed05a70d720f31ed2f144f64ae8eadcf66f157
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-04-19 19:28:02 -0600

    container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,13 +1,24 @@
 import { CSSProperties, PropsWithChildren } from 'react';
 import styled from 'styled-components';
 
-const StyledContainer = styled.div``;
+const StyledContainer = styled.div`
+  padding: 1rem;
+  margin: 0 auto;
+
+  width: 100%;
+
+  @media (min-width: 576px) {
+    width: 90%;
+
+    padding: 1rem 0;
+  }
+`;
 
 type ContainerProps = PropsWithChildren & {
   style?: CSSProperties;
 };
 
-const Container: React.FC = ({ children, style }: ContainerProps) => {
+const Container = ({ children, style }: ContainerProps) => {
   return <StyledContainer style={style}>{children}</StyledContainer>;
 };
 

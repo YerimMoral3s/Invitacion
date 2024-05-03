12da196db830d710300a0eda230246b0eb63c4f5
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-05-02 18:07:12 -0600

    general info

---
+++ src/components/GeneralText.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import Container from './Container';
+import Text from './Text';
+
+const StyledGeneralText = styled.div`
+  .container-ImageCallAction {
+    text-align: center;
+
+    p {
+      font-family: rosarivo-italic;
+      font-size: 1.5rem;
+      margin: 1rem 0;
+    }
+    p:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+export default function GeneralText(props: { texts: string[] }) {
+  return (
+    <StyledGeneralText>
+      <Container className="container-ImageCallAction">
+        {props.texts.map((text, index) => (
+          <Text key={index} text={text} as="p" />
+        ))}
+      </Container>
+    </StyledGeneralText>
+  );
+}

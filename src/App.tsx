a81b62e89a1979634d37c1fdb1f197f6ed380ef7
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-05-02 18:08:28 -0600

    body component

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,164 @@
+import Body from './sections/Body';
 import NuestraHistoria from './sections/NuestraHistoria';
 import PhotosAnimation from './sections/PhotosAnimation';
+
+import logoPlace from './assets/images/place.png';
+import logoFinca from './assets/images/finca.png';
+import icon1 from './assets/images/contails.png';
+import icon2 from './assets/images/sign.png';
+import icon3 from './assets/images/brindis.png';
+import icon4 from './assets/images/IGLESIA.png';
+import logoLiverpool from './assets/images/liverpool.png';
+import logoPalacio from './assets/images/palacio.png';
+import img from './assets/images/irv-son-14.jpg';
+
+const CivilObjects = [
+  {
+    name: 'MainTitle',
+    text: 'Lugar y Fecha',
+    style: {
+      width: '100%',
+      textAlign: 'center',
+    },
+  },
+  {
+    name: 'Date',
+    text: 'Ceremonia Civil',
+    month: 'MARZO',
+    day: '01',
+    year: '2025',
+  },
+
+  {
+    name: 'ImageCallAction',
+    image: logoPlace,
+    alt: 'Logo Place',
+    buttonText: 'Ver Ubicación',
+    href: 'https://goo.gl/maps/7Q3',
+  },
+
+  {
+    name: 'Schedules',
+    element1: {
+      img: icon1,
+      alt: 'Coctél de Bienvenida',
+      title: 'Coctél de Bienvenida',
+      schedule: '6:30 PM',
+    },
+    element2: {
+      img: icon2,
+      alt: 'Ceremonia Civil',
+      title: 'Ceremonia Civil',
+      schedule: '7:30 PM',
+    },
+  },
+
+  {
+    name: 'Separator',
+    title: 'Coctél',
+    href: 'https://www.google.com/maps/dir//formal',
+  },
+];
+
+const ReligiosaObjects = [
+  {
+    name: 'MainTitle',
+    text: 'Lugar y Fecha',
+    style: {
+      width: '100%',
+      textAlign: 'center',
+    },
+  },
+  {
+    name: 'Date',
+    text: 'Ceremonia Religiosa',
+    month: 'MARZO',
+    day: '15',
+    year: '2025',
+  },
+
+  {
+    name: 'ImageCallAction',
+    image: logoFinca,
+    alt: 'Logo Place',
+    buttonText: 'Ver Ubicación',
+    href: 'https://goo.gl/maps/7Q3',
+  },
+
+  {
+    name: 'Schedules',
+    element1: {
+      img: icon4,
+      alt: 'Ceremonia Religiosa',
+      title: 'Ceremonia Religiosa',
+      schedule: '4:30 PM',
+    },
+    element2: {
+      img: icon3,
+      alt: 'Celebraccion',
+      title: 'Celebraccion',
+      schedule: '6:00 PM',
+    },
+  },
+
+  {
+    name: 'Separator',
+    title: 'Formal',
+    href: 'https://www.google.com/maps/dir//formal',
+  },
+];
+
+const GiftObjects = [
+  // {
+  //   name: 'Imagen',
+  //   src: img,
+  //   alt: 'Imagen',
+  //   style: {
+  //     width: '100%',
+  //     height: '500px',
+  //     objectFit: 'cover',
+  //     filter: 'grayscale(1)',
+  //   },
+  // },
+  {
+    name: 'GeneralText',
+    texts: [
+      'Tu compañía es nuestro mejor regalo, pero si gustas tener un detalle con nosotros. Te dejamos estas sugerencias.',
+      '¡GRACIAS!',
+    ],
+  },
+  {
+    name: 'ImageCallAction',
+    image: logoLiverpool,
+    alt: 'Logo Place',
+    buttonText: 'Ver Mesa de Regalos',
+    href: 'https://goo.gl/maps/7Q3',
+  },
+  {
+    name: 'ImageCallAction',
+    image: logoPalacio,
+    alt: 'Logo Place',
+    buttonText: 'Ver Mesa de Regalos',
+    href: 'https://goo.gl/maps/7Q3',
+  },
+  {
+    name: 'MainTitle',
+    text: 'Opciones de Hospedaje',
+    style: {
+      width: '100%',
+      textAlign: 'center',
+    },
+  },
+];
 
 function App() {
   return (
     <>
       <PhotosAnimation />
       <NuestraHistoria />
-      {/* <div
-        className="cont"
-        style={{
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'black',
-        }}
-      >
-        <h1>Hola</h1>
-      </div>
-      <div
-        className="cont"
-        style={{
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'black',
-        }}
-      >
-        <h1>Hola</h1>
-      </div>
-      <div
-        className="cont"
-        style={{
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'black',
-        }}
-      >
-        <h1>Hola</h1>
-      </div>
-      <div
-        className="cont"
-        style={{
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'black',
-        }}
-      >
-        <h1>Hola</h1>
-      </div>
-      <div
-        className="cont"
-        style={{
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'black',
-        }}
-      >
-        <h1>Hola</h1>
-      </div>
-      <div
-        className="cont"
-        style={{
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'black',
-        }}
-      >
-        <h1>Hola</h1>
-      </div>
-      <div
-        className="cont"
-        style={{
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'black',
-        }}
-      >
-        <h1>Hola</h1>
-      </div>
-      <div
-        className="cont"
-        style={{
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'black',
-        }}
-      >
-        <h1>Hola</h1>
-      </div>
-      <div
-        className="cont"
-        style={{
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'black',
-        }}
-      >
-        <h1>Hola</h1>
-      </div>
-      <div
-        className="cont"
-        style={{
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'black',
-        }}
-      >
-        <h1>Hola</h1>
-      </div>
-      <div
-        className="cont"
-        style={{
-          height: '100px',
-          width: '100px',
-          backgroundColor: 'black',
-        }}
-      >
-        <h1>Hola</h1>
-      </div> */}
+      <Body InfoList={CivilObjects} />
+      <Body InfoList={ReligiosaObjects} />
+      <Body InfoList={GiftObjects} />
     </>
   );
 }
---
+++ src/sections/Body.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import MainTitle from '../components/MainTitle';
+import Date from '../components/Date';
+import ImageCallAction from '../components/ImageCallAction';
+import Schedules from '../components/Schedules';
+import Separator from '../components/Separator';
+
+import cloud from '../assets/images/cloud.png';
+import Imagen from '../components/Imagen';
+import GeneralText from '../components/GeneralText';
+
+const StyledCivilCeremony = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background:
+    url(${cloud}) no-repeat left -300px top 0px,
+    url(${cloud}) no-repeat right -300px bottom -120px;
+
+  background-size: 700px;
+
+  @media (min-width: 450px) {
+    background-size: 800px;
+  }
+
+  @media (min-width: 576px) {
+    background:
+      url(${cloud}) no-repeat left -300px top 0px,
+      url(${cloud}) no-repeat right -300px bottom -150px;
+    background-size: 900px;
+  }
+
+  @media (min-width: 768px) {
+    background-size: 1000px;
+  }
+
+  @media (min-width: 992px) {
+    background-size: 1100px;
+  }
+
+  @media (min-width: 1200px) {
+    background:
+      url(${cloud}) no-repeat left -500px top -200px,
+      url(${cloud}) no-repeat right -500px bottom -100px;
+
+    background-size: 1200px;
+  }
+
+  // *{
+  //   border: 1px solid red;
+  // }
+`;
+
+const Components = [
+  MainTitle,
+  Date,
+  ImageCallAction,
+  Schedules,
+  Separator,
+  Imagen,
+  GeneralText,
+];
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function CivilCeremony(props: { InfoList: any[] }) {
+  const Comps = props.InfoList.map((element, idx) => {
+    const Component = Components.find((c) => c.name === element.name);
+
+    if (Component) {
+      const { name, ...rest } = element;
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return <Component {...(rest as any)} id={name} key={name + '_' + idx} />;
+    }
+    return <></>;
+  });
+
+  return <StyledCivilCeremony>{Comps}</StyledCivilCeremony>;
+}
+
+export default CivilCeremony;

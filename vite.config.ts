8a8a412ee4d0b6443a0dce69cabf541733a60d4d
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-08-08 18:25:26 -0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://yerimmoral3s.github.io/Inivitacion/',
+  base: '/', // Cambia la base a '/'
+  // base: 'https://yerimmoral3s.github.io/Inivitacion/',
 });

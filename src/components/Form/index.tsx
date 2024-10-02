e573567d3e60d7a12d07a4af6e2f361e8ff94c8c
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-10-01 21:12:49 -0600

    re-factor

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -19,161 +19,129 @@ import { SubGuest } from '../sdk/types';
 export default function Form() {
   const user = useUser();
   const update = useUpdateUser();
-  const loaderRef = useRef<LoadingBarRef>({} as LoadingBarRef);
+  const loaderRef = useRef<LoadingBarRef>(null);
   const updateGuestCache = useUpdateGuestCache();
   const updateGuestAPI = useUpdateGuestAPI();
 
-  const notifyError = () => toast.error('Algo Salio mal :(');
-  const notifySucces = () => toast.success('¡Guardado!');
+  const notifyError = () => toast.error('Algo salió mal :(');
+  const notifySuccess = () => toast.success('¡Guardado!');
 
   const updateSubGuest = (subGuest: SubGuest) => {
-    if (!user.data?.attributes.sub_guests) {
-      return;
-    }
+    if (!user.data?.attributes.sub_guests) return;
+
     updateGuestCache.updateCache(subGuest, !subGuest.attributes.confirmation);
     debouncedUpdate(user.data.attributes.sub_guests.data);
   };
 
-  const debouncedUpdate = useDebouncedCallback(async (subGuest: SubGuest[]) => {
-    try {
-      if (!user.data) {
-        return;
+  const debouncedUpdate = useDebouncedCallback(
+    async (subGuests: SubGuest[]) => {
+      if (!user.data) return;
+
+      try {
+        loaderRef.current?.continuousStart();
+        const data = subGuests.map((sg) => ({
+          id: sg.id,
+          confirmation: sg.attributes.confirmation,
+        }));
+
+        const req: updateSubGuestRequest = {
+          id: user.data.id,
+          sub_guests: data,
+        };
+
+        const res = await updateGuestAPI.mutateAsync(req);
+
+        if (res) notifySuccess();
+        loaderRef.current?.complete();
+      } catch (error) {
+        notifyError();
+        loaderRef.current?.complete();
+        console.error(error);
       }
-      loaderRef.current.continuousStart();
-      const data = subGuest.map((sg) => ({
-        id: sg.id,
-        confirmation: sg.attributes.confirmation,
-      }));
-      const req: updateSubGuestRequest = {
-        id: user.data?.id,
-        sub_guests: data,
-      };
-      const res = await updateGuestAPI.mutateAsync(req);
-      if (res) {
-        notifySucces();
-        loaderRef.current.complete();
+    },
+    1000,
+  );
+
+  const confirmAttendance = useDebouncedCallback(
+    async (state: boolean, type: 'civil' | 'religious') => {
+      if (!user.data) return;
+
+      try {
+        loaderRef.current?.continuousStart();
+
+        if (shouldCancelAll(state, type)) cancelAll();
+
+        const updates =
+          type === 'civil'
+            ? { civil_confirmation: state }
+            : { religious_confirmation: state };
+
+        const res = await update.mutateAsync({ id: user.data.id, updates });
+
+        if (res) notifySuccess();
+        loaderRef.current?.complete();
+      } catch (error) {
+        notifyError();
+        loaderRef.current?.complete();
+        console.error(error);
       }
-    } catch (error) {
-      notifyError();
-      loaderRef.current.complete();
-      console.error(error);
-    }
-  }, 1000);
+    },
+    300,
+  );
 
-  const confirmReligious = useDebouncedCallback(async (state: boolean) => {
-    try {
-      if (!user.data) {
-        return;
-      }
+  const shouldCancelAll = (state: boolean, type: 'civil' | 'religious') => {
+    const { religious_confirmation, civil_confirmation } =
+      user.data?.attributes || {};
 
-      loaderRef.current.continuousStart();
+    if (
+      type === 'civil' &&
+      religious_confirmation !== null &&
+      civil_confirmation !== null &&
+      !religious_confirmation &&
+      !state
+    )
+      return true;
 
-      if (
-        user.data.attributes.religious_confirmation !== null &&
-        user.data.attributes.civil_confirmation !== null &&
-        !user.data.attributes.civil_confirmation &&
-        !state
-      ) {
-        cancelAll();
-      }
+    if (
+      type === 'religious' &&
+      religious_confirmation !== null &&
+      civil_confirmation !== null &&
+      !civil_confirmation &&
+      !state
+    )
+      return true;
 
-      const res = await update.mutateAsync({
-        id: user.data.id,
-        updates: {
-          religious_confirmation: state,
-        },
-      });
-
-      if (res) {
-        notifySucces();
-        loaderRef.current.complete();
-      }
-    } catch (error) {
-      notifyError();
-
-      loaderRef.current.complete();
-      console.error(error);
-    }
-  }, 300);
-
-  const confirmCivil = useDebouncedCallback(async (state: boolean) => {
-    try {
-      if (!user.data) {
-        return;
-      }
-      if (update.isPending) {
-        return;
-      }
-      loaderRef.current.continuousStart();
-
-      if (
-        user.data.attributes.religious_confirmation !== null &&
-        user.data.attributes.civil_confirmation !== null &&
-        !user.data.attributes.religious_confirmation &&
-        !state
-      ) {
-        cancelAll();
-      }
-
-      const res = await update.mutateAsync({
-        id: user.data.id,
-        updates: {
-          civil_confirmation: state,
-        },
-      });
-
-      if (res) {
-        notifySucces();
-        loaderRef.current.complete();
-      }
-    } catch (error) {
-      notifyError();
-
-      loaderRef.current.complete();
-      console.error(error);
-    }
-  }, 300);
+    return false;
+  };
 
   const cancelAll = () => {
-    try {
-      if (!user.data) {
-        return;
-      }
-      const subGuest = user.data.attributes.sub_guests.data;
+    if (!user.data) return;
 
+    try {
+      const subGuest = user.data.attributes.sub_guests.data;
       const data = subGuest.map((sg) => ({
         id: sg.id,
         confirmation: false,
       }));
 
       const req: updateSubGuestRequest = {
-        id: user.data?.id,
+        id: user.data.id,
         sub_guests: data,
       };
 
       updateGuestAPI.mutate(req);
     } catch (error) {
       notifyError();
-
       console.error(error);
     }
   };
 
-  if (user.error || !user.data) {
-    return;
-  }
-  const foreverAlone =
-    ' por favor indícanos si podrás acompañarnos en este día tan especial.';
-  const noForeveAlone =
-    ' para poder confirmar tu asistencia es importante que nos indiques quienes de las siguientes personas registradas, asistirán.';
-
   const getMessage = () => {
-    const civil_confirmation = user.data.attributes.civil_confirmation;
-    const religious_confirmation = user.data.attributes.religious_confirmation;
+    const { civil_confirmation, religious_confirmation } =
+      user.data?.attributes || {};
 
-    if (civil_confirmation === null || religious_confirmation === null) {
+    if (civil_confirmation === null || religious_confirmation === null)
       return '';
-    }
 
     if (!civil_confirmation && religious_confirmation) {
       return (
@@ -184,6 +152,7 @@ export default function Form() {
         </>
       );
     }
+
     if (civil_confirmation && !religious_confirmation) {
       return (
         <>
@@ -193,6 +162,7 @@ export default function Form() {
         </>
       );
     }
+
     if (!civil_confirmation && !religious_confirmation) {
       return (
         <>
@@ -212,113 +182,71 @@ export default function Form() {
     );
   };
 
+  if (!user.data || user.error) return null;
+
+  const subGuests = user.data?.attributes.sub_guests?.data || [];
+
   return (
     <>
       <LoadingBar color={colors.blue} ref={loaderRef} />
       <Toaster />
-
       <StyledForm>
         <Container>
           <h1>CONFIRMACIÓN</h1>
-
           <p>
             ERES MUY IMPORTANTE PARA NOSOTROS, ESPERAMOS CONTAR CON TU COMPAÑÍA
             EN ESTE DÍA TAN ESPECIAL
           </p>
           <p>
-            ¡Hola {user.data?.attributes.name}!,{' '}
-            {user?.data?.attributes?.sub_guests?.data.length > 0
-              ? noForeveAlone
-              : foreverAlone}
+            ¡Hola {user.data.attributes.name}!,{' '}
+            {subGuests.length > 0 ? noForeverAlone : foreverAlone}
           </p>
-          {user?.data?.attributes?.sub_guests?.data.length > 0 ? (
+
+          {subGuests.length > 0 && (
             <>
               <p>
                 Recuerda que por seguridad, solo podrán asistir las personas que
                 estén registradas y confirmadas.
               </p>
-
               <div
                 className="names"
                 style={{
                   justifyContent:
-                    user?.data?.attributes?.sub_guests.data.length > 3
-                      ? 'space-evenly'
-                      : 'center',
+                    subGuests.length > 3 ? 'space-evenly' : 'center',
                 }}
               >
-                {user.data?.attributes.sub_guests.data.map((subGuest, idx) => {
-                  return (
-                    <Name
-                      disabled={
-                        user.data.attributes.religious_confirmation !== null &&
-                        user.data.attributes.civil_confirmation !== null &&
-                        !user.data.attributes.civil_confirmation &&
-                        !user.data.attributes.religious_confirmation
-                      }
-                      subGuest={subGuest}
-                      key={idx}
-                      user={user.data.id}
-                      onClick={updateSubGuest}
-                    />
-                  );
-                })}
+                {subGuests.map((subGuest, idx) => (
+                  <Name
+                    disabled={
+                      user.data.attributes.religious_confirmation === false &&
+                      user.data.attributes.civil_confirmation === false
+                    }
+                    subGuest={subGuest}
+                    key={idx}
+                    user={user.data.id}
+                    onClick={updateSubGuest}
+                  />
+                ))}
               </div>
             </>
-          ) : null}
+          )}
 
           <div className="confirm-container">
-            <div className="confirm-item" style={{ marginBottom: '1rem' }}>
-              <h3>¿Asistirás a la ceremonia religiosa?</h3>
-              <div className="item-btns">
-                <button
-                  onClick={() => confirmReligious(true)}
-                  disabled={
-                    user.data.attributes.religious_confirmation !== null &&
-                    !!user.data.attributes.religious_confirmation
-                  }
-                >
-                  CONFIRMAR <br />
-                  ASISTENCIA
-                </button>
-                <button
-                  onClick={() => confirmReligious(false)}
-                  disabled={
-                    user.data.attributes.religious_confirmation !== null &&
-                    !user.data.attributes.religious_confirmation
-                  }
-                >
-                  NO PODRÉ <br />
-                  ASISTIR
-                </button>
-              </div>
-            </div>
-            <div className="confirm-item">
-              <h3>¿Asistirás a la ceremonia civil?</h3>
-              <div className="item-btns">
-                <button
-                  onClick={() => confirmCivil(true)}
-                  disabled={
-                    user.data.attributes.religious_confirmation !== null &&
-                    !!user.data.attributes.civil_confirmation
-                  }
-                >
-                  CONFIRMAR <br />
-                  ASISTENCIA
-                </button>
-                <button
-                  onClick={() => confirmCivil(false)}
-                  disabled={
-                    user.data.attributes.civil_confirmation !== null &&
-                    !user.data.attributes.civil_confirmation
-                  }
-                >
-                  NO PODRÉ <br />
-                  ASISTIR
-                </button>
-              </div>
-            </div>
+            <ConfirmItem
+              label="¿Asistirás a la ceremonia religiosa?"
+              confirmAction={() => confirmAttendance(true, 'religious')}
+              declineAction={() => confirmAttendance(false, 'religious')}
+              confirmed={user.data.attributes.religious_confirmation}
+            />
+
+            <ConfirmItem
+              label="¿Asistirás a la ceremonia civil?"
+              confirmAction={() => confirmAttendance(true, 'civil')}
+              declineAction={() => confirmAttendance(false, 'civil')}
+              confirmed={user.data.attributes.civil_confirmation}
+            />
           </div>
+
           <div className="copy">
             <p>
               Por favor, confirma tu asistencia antes del 15 de diciembre 2024.
@@ -331,6 +259,30 @@ export default function Form() {
     </>
   );
 }
+
+const ConfirmItem = ({
+  label,
+  confirmAction,
+  declineAction,
+  confirmed,
+}: {
+  label: string;
+  confirmAction: () => void;
+  declineAction: () => void;
+  confirmed: boolean | null;
+}) => (
+  <div className="confirm-item" style={{ marginBottom: '1rem' }}>
+    <h3>{label}</h3>
+    <div className="item-btns">
+      <button onClick={confirmAction} disabled={confirmed === true}>
+        CONFIRMAR <br /> ASISTENCIA
+      </button>
+      <button onClick={declineAction} disabled={confirmed === false}>
+        NO PODRÉ <br /> ASISTIR
+      </button>
+    </div>
+  </div>
+);
 
 const StyledForm = styled.div`
   background: ${colors.gray};
@@ -347,22 +299,22 @@ const StyledForm = styled.div`
 
   .names {
     display: flex;
-    // justify-content: space-evenly;
     flex-wrap: wrap;
   }
+
   .confirm-container {
-    margin: 2rem 0 0 0;
+    margin-top: 2rem;
   }
 
   .copy {
     background: ${colors.cream};
     color: ${colors.gray};
-    margin: 2rem 0 0 0;
+    margin-top: 2rem;
     padding: 1rem;
+
     p {
       border: 2px solid ${colors.gray};
       padding: 1rem;
-      margin: 0;
     }
   }
 
@@ -379,7 +331,7 @@ const StyledForm = styled.div`
       letter-spacing: 5px;
       padding: 0.8rem;
       margin: 0.8rem;
-      border: 0;
+      border: none;
       border-radius: 10px;
       font-family: 'Montserrat', sans-serif;
       font-size: 0.8em;
@@ -392,16 +344,18 @@ const StyledForm = styled.div`
   }
 `;
 
+const foreverAlone =
+  ' por favor indícanos si podrás acompañarnos en este día tan especial.';
+const noForeverAlone =
+  ' para poder confirmar tu asistencia es importante que nos indiques quienes de las siguientes personas registradas, asistirán.';
+
 const mensajes = {
   confirmacion_ambas:
     '¡Gracias por confirmar tu asistencia a ambas ceremonias! Estamos felices de que seas parte de estos momentos tan especiales. ¡Nos vemos pronto para celebrar juntos!',
-
   confirmacion_solo_civil:
     '¡Gracias por confirmar tu asistencia a nuestra ceremonia civil! Estamos emocionados de compartir este momento especial contigo.',
-
   confirmacion_solo_religiosa:
-    '¡Gracias por confirmar tu asistencia a nuestra boda! Será maravilloso compartir esta bendición contigo.',
-
+    '¡Gracias por confirmar tu asistencia a nuestra ceremonia religiosa! Será maravilloso compartir esta bendición contigo.',
   no_asistira:
     'Lamentamos que no puedas acompañarnos, pero entendemos. Esperamos poder compartir momentos contigo en el futuro. ¡Gracias por hacernos saber!',
 };

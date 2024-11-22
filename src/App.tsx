519a43e4aabd45cf0fba4f77e35cab3ce4d9aa63
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-11-21 23:30:19 -0600

    createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,52 @@
-import Banner from './components/Banner';
-import Footer from './components/Footer';
-import Form from './components/Form';
-import Gallery from './components/Gallery';
-import GiftTable from './components/GiftTable';
-import Hospitalities from './components/Hospitalities';
-import Itinerario from './components/Itinerario';
-import { Loader } from './components/Loader';
-import NosCasamos from './components/NosCasamos';
-import Nosotros from './components/Nosotros';
-import Spoty from './components/Spoty';
-import PhotosAnimation from './sections/PhotosAnimation';
+import React, { Suspense, lazy } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Loader } from './components/Loader/Loader';
+
+// Importar componentes
+const Invitation = lazy(() => import('./Invitation'));
+const Admin = lazy(() => import('./Admin'));
+const Directory = lazy(() => import('./Directory')); // Define tu componente Directory
+const Charts = lazy(() => import('./Charts')); // Define tu componente Charts
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Suspense fallback={<Loader />}>
+        <Invitation />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/admin',
+    element: (
+      <Suspense fallback={<Loader />}>
+        <Admin />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: '', // Subruta predeterminada para /admin
+        element: (
+          <Suspense fallback={<Loader />}>
+            <Directory />
+          </Suspense>
+        ),
+      },
+      {
+        path: 'charts', // Subruta para /admin/charts
+        element: (
+          <Suspense fallback={<Loader />}>
+            <Charts />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <Loader>
-      <PhotosAnimation />
-      <NosCasamos />
-      <Nosotros />
-      <Banner />
-      <Itinerario />
-      <GiftTable />
-      <Gallery />
-      <Hospitalities />
-      <Spoty />
-      <Form />
-      <Footer />
-    </Loader>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
